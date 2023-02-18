6e1c6da76e86aee09e02da6a3566d50a196c9ce9
slipgoat <slipgoat@gmail.com>
2023-02-18 11:09:13 +0200

    fixed period boundaries

--- utils.py
+++ utils.py
@@ -14,5 +14,5 @@ def get_current_period() -> tuple:
 def get_period(offset_months: int) -> tuple:
     now = datetime.utcnow()
     start_date = date(now.year, now.month, 1) + relativedelta(months=offset_months)
-    end_date = start_date + relativedelta(months=1) - timedelta(days=1)
+    end_date = start_date + relativedelta(months=1)
     return start_date, end_date
